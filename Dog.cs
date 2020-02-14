3fb147fcdc0a037680c898b9615baebea2cf25aa
Natalie Myers <nataliehmyers@gmail.com>
2020-02-14 15:33:26 -0600

    Complete program Dog

--- Dog.cs
+++ Dog.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+namespace Gender { 
+    public enum Gender {Male, Female};
+    class Dog {
+        public string name;
+        public string owner;
+        public int age;
+        public Gender gender;
+        public Dog (string name, string owner, int age, Gender gender) {
+            this.name = name;
+            this.owner = owner;
+            this.age = age;
+            this.gender = gender;
+        }
+        public void Bark(int times) {
+            for(int i = 0; i < times; i++) {
+                Console.Write("Woof!");
+            }
+            Console.WriteLine();
+        }
+        public string GetTag() {
+            string tag = "If lost, call " + owner + ". ";
+            if (gender == 0) {
+                tag += "His name is " + name + " he is " + age.ToString();
+            } else {
+                tag += "Her name is " + name + " she is " + age.ToString();
+            } 
+            if (age > 1) {
+                tag += " years old.";
+            } else {
+                tag += " year old.";
+            }
+            return tag;
+        }
+    }
+    class Program {
+        static void Main(string[] args) {
+            Dog puppy = new Dog("Orion", "Shawn", 1, Gender.Male);
+            puppy.Bark(3);
+            Console.WriteLine(puppy.GetTag());
 
-namespace Dog
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+            Dog myDog = new Dog("Lileu", "Dale", 4, Gender.Female);
+            myDog.Bark(1);
+            Console.WriteLine(myDog.GetTag());
         }
     }
 }
